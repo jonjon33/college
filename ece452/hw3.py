f9cecdcdc1c05038a36d0cb0481ffb6e7001ff7c
Jon Hanson <jon33@outlook.com>
2017-12-24 16:56:32 -0800

    adding in hw3 boilerplate

---
+++ ece452/hw3.py
@@ -0,0 +1,18 @@
+# jon hanson
+# may 2, 2017
+# ece 452
+# hw3 - quantization
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def chiSquare():
+    return
+
+def q():
+    return
+
+def R():
+    return
+
